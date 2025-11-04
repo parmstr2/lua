0ce1e0e25fe7ba42bb0515a3e25f49772c748865
parmstr2 <paul.armstrong0625@gmail.com>
2025-11-04 00:18:24 +0000

    Adjusting transfer rate logic

--- powerPayment.lua
+++ powerPayment.lua
@@ -2,6 +2,8 @@ local energy = peripheral.find("energy_detector")
 local barrel = peripheral.find("minecraft:barrel")
 local trash = peripheral.find("trashcans:item_trash_can_tile")
 
+local timeMult = 1
+
 function scanPayment()
     local items = barrel.list()
     local payment = 0
@@ -11,14 +13,19 @@ function scanPayment()
             payment = payment + items[i].count
         elseif (items[i].name == "numismatics:bevel") then
             payment = payment + (items[i].count * 8)
+            timeMult = 10
         elseif (items[i].name == "numismatics:sprocket") then
             payment = payment + (items[i].count * 16)
+            timeMult = 100
         elseif (items[i].name == "numismatics:cog") then
             payment = payment +(items[i].count * 64)
+            timeMult = 1000
         elseif (items[i].name == "numismatics:crown") then
             payment = payment + (items[i].count * 512)
+            timeMult = 10000
         elseif (items[i].name == "numismatics:sun") then
             payment = payment + (items[i].count * 4096)
+            timeMult = 100000
         end
     end
 
@@ -37,8 +44,8 @@ while true do
             end
         end
     end
-    for i = 1, payment do
-        energy.setTransferRateLimit(480)
+    for i = 1, 10 do
+        energy.setTransferRateLimit(48 * timeMult)
         sleep(0.95)
     end
     energy.setTransferRateLimit(0)
